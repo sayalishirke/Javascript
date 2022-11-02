e01032b68c6c2b94e445480a32ac74078791db60
Sayali Shirke <sayalishirke27@gmail.com>
2022-11-02 16:12:23 +0530

    inital

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
-localStorage.getItem("activelink")
-  const links = document.getElementById("list")
+
+const links = document.getElementById("list")
 if(links!==null){
   links.addEventListener('click', function(e) { 
     
@@ -8,15 +8,14 @@ if(links!==null){
     if (elementsLi.length>0){
       elementsLi[0].classList.remove("active")
     }
-    // activelink=localStorage.getItem("activelink")
-    // element=document.getElementById(activelink)
+    
     
     e.target.classList.add("active")
-    localStorage.setItem("activelink",document.getElementsByClassName("active")[0].innerHTML)
-    //localStorage.setItem("activelink", e.target.text)
-    // e.target.classList.remove("active")
+    
+    localStorage.setItem("activelink", e.target.text)
+    
 
-    //localStorage.setItem("activelinkclasses", e.target.classList)
+    localStorage.setItem("activelinkclasses", e.target.classList)
     
     
   },
@@ -25,47 +24,14 @@ if(links!==null){
 }
 
 
-// const links = document.getElementById("list")
-// if(links!==null){
-//   links.addEventListener('click', function(e) { 
-    
-//     const elementsLi = document.querySelectorAll('a.active')
-    
-//     if (elementsLi.length>0){
-//       elementsLi[0].classList.remove("active")
-//     }
-//     // activelink=localStorage.getItem("activelink")
-//     // element=document.getElementById(activelink)
-    
-//     e.target.classList.add("active")
-    
-//     localStorage.setItem("activelink", e.target.text)
-//     // e.target.classList.remove("active")
-
-//     //localStorage.setItem("activelinkclasses", e.target.classList)
-    
-    
-//   },
-//   )
-// }
-
-// window.onload=(e)=>{
-//  // const x = document.getElementsByClassName("active")[0].innerHTML
-//   localStorage.setItem("activelink",document.getElementsByClassName("active")[0].innerHTML)
-//   xyz()
-// activelink=localStorage.getItem("activelink"),
-// classes=localStorage.getItem("activelinkclasses")
-// element=document.getElementById(activelink)
-// element.classList.add("active")
-
-// }
 
 
 
-// // activelink=localStorage.getItem("activelink"),
-// // classes=localStorage.getItem("activelinkclasses")
-// // element=document.getElementById(activelink)
-// // element.classList.add("active")
+
+activelink=localStorage.getItem("activelink"),
+classes=localStorage.getItem("activelinkclasses")
+element=document.getElementById(activelink)
+element.classList.add("active")
 
 
 
